300da8a468af3772196b40cf5146a2ff81a45f20
Dez <dezd24@gmail.com>
2022-10-07 17:14:17 -0700

    Friday

---
+++ script.js
@@ -0,0 +1,34 @@
+//* STATE - What do we want to keep track of?
+/***************************************************/
+var playAgain;
+var userChoice = "";
+var cpuChoice = "";
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+var OPTIONS = ["Rock", "Paper", "Scissors"];
+
+//* ACTIONS - What are the steps?
+
+/*
+1. Prompt user to play 
+2. User makes choice  (rock, paper, scissors)
+3. Store value for cpus response and for future data recall
+4. Computer makes choice
+5. Computers choice is displayed to user and stored for future data recall
+6. Compare user choice to computer choice
+7. Display results
+   - if user picks "Rock":
+     - if cpu picks "Rock": tie
+        - if cpu picks "Paper": loss
+        - if cpu picks "Scissors": win
+
+    - if user picks "Paper":
+        - if cpu picks "Rock": wins
+        - if cpu picks "Paper": tie
+
+    - if user picks "Scissors":
+        - if cpu picks "Rock": loss
+        - if cpu picks "Paper": win
+8. Display score
